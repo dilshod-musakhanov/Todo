8e39fdcf872ccb42bfcb4bd65289911ba4f8b687
Dilshod Musakhanov <musakhanov@yahoo.com>
2021-12-19 03:28:35 +0400

    add filter

--- app.js
+++ app.js
@@ -1,70 +1,85 @@
 //selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
-//event listener
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
+//event listeners
 
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
 
 //functions
 
-function addTodo(event){
-    event.preventDefault();
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add("todo");
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+function addTodo(e) {
+  //prevent natural behaviour
+  e.preventDefault();
+  //create todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create list
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
 
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add('completed-btn');
-    todoDiv.appendChild(completedButton);
-    
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('trash-btn');
-    todoDiv.appendChild(trashButton);
-
-    todoList.appendChild(todoDiv);
-    todoInput.value = "";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  todoInput.value = "";
+  //create Completed Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //create trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //attach final Todo
+  todoList.appendChild(todoDiv);
 }
 
-function deleteCheck(e) {
-    const item = e.target;
+function deleteTodo(e) {
+  const item = e.target;
 
-    if(item.classList[0] === "trash-btn") {
-        const todo = item.parentElement; 
-        //animation
-        todo.classList.add("fall");
-        todo.addEventListener('transitionend', ()=> {
-            todo.remove();
-        })
-
-        
-    }
-
-    if(item.classList[0] === "completed-btn"){
-        const todo = item.parentElement;
-        todo.classList.add('completed');
-    }
+  if (item.classList[0] === "trash-btn") {
+    // e.target.parentElement.remove();
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    //at the end
+    todo.addEventListener("transitionend", e => {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    console.log(todo);
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+}
 
 
