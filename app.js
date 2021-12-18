d75a6375818bb1467a36cd6c3c71e44cc4881af2
Dilshod Musakhanov <musakhanov@yahoo.com>
2021-12-18 11:21:46 +0400

    give some style to todo items

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function addTodo(event){
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
     const newTodo = document.createElement("li");
-    newTodo.innerText = 'Salomlar';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
 
